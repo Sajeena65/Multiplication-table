4e92174d89578025fd01912edf27289739c256b0
Sajeena <Sajeena>
2016-07-18 21:36:14 +0545

    First Commit

---
+++ src/com/leapfrog/multable/program.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leapfrog.multable;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      int x,y,i;
+      Scanner in = new Scanner(System.in);
+      System.out.println("Enter an integer to print it's multiplication table:");
+      x= in.nextInt();
+      System.out.println("Enter the range of the multiplication table:");
+      y= in.nextInt();
+      System.out.println("Multiplication table of is :-");
+ 
+      for ( i = 1 ; i<= y ; i++ )
+         System.out.println(x+"*"+i+" = "+(x*i));
+       
+    }
+    
+}
